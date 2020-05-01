4c14a259c7ced0f7df87c4239d055b4f095c8f5c
Brian Tai <btai@soxhub.com>
2020-05-01 16:59:10 -0700

    fix lan's broken shiet

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-
-while ! nc -z localhost 8080; do   
+while [ -z $(netstat -lnt | awk '$4 ~ /:8080$/') ]; do
   echo "Waiting for tap to launch on 8080..."
   sleep 1 # wait for 1/10 of the second before check again
 done
